30d5e1aa192a27cc52b3edcbdd1d41256dd0ca4b
sasi <sasiinweb@gmail.com>
2026-08-15 16:30:06 +0530

    comparing number with equal number of digits

--- math_book/6th/Comparing_numbers.py
+++ math_book/6th/Comparing_numbers.py
@@ -2,10 +2,6 @@
 #1.4.2 Comparing numbers with unequal number of digits
 
 
-\
-# here problem is how do i know the user input length. ok we may choose the find Len() . but how we 
-# ,  wait, letss try the 4 digit given steps on book.
-
 place_names = ('ones','tens','hundreds','thousands','ten thousands','lakhs','Ten lakhs','Crores','Ten crores','Hundred crores','Thousand crores')
 place_values = (1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000)
 
@@ -28,3 +24,11 @@ for top_numbers in first_number:
     elif top_numbers < second_number[i]:
         print(f'The second_number {second_number} is greater than\nThe first_number {first_number}')
         break
+        
+        
+#ruff:
+# 
+# here problem is how do i know the user input length. ok we may choose the find Len() . # # but how we 
+#,  wait, letss try the 4 digit given steps on book.
+
+
